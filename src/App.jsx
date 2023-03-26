87fb2b2fc7507716c160edb2f4882281ea620eed
Shivang Sharma <85074821+shivang821@users.noreply.github.com>
2023-03-26 14:14:14 +0530

    add icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import AddHomeWorkIcon from '@mui/icons-material/AddHomeWork';
 function App() {
 
   return (
     <div className="App">
       <h1>hello</h1>
+      <AddHomeWorkIcon/>
     </div>
   )
 }
